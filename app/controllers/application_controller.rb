5727e3df76d86f4d9c7471a331e273f50b966756
okkez <okkez000@gmail.com>
2009-12-07 00:44:28 +0900

    move methods from ApplicationController to MultiAuth::{InstanceMethods,ClassMethods}

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,43 +8,4 @@ class ApplicationController < ActionController::Base
   GetText.locale = "ja"
   init_gettext "multi_auth"
 
-  private
-
-  def self.verify_method_post(options = {})
-    verify({
-      :method => :post,
-      :render => {:text => "Method Not Allowed", :status => 405},
-    }.merge(options))
-  end
-
-  def authentication(user_id = session[:user_id])
-    @login_user = User.find_by_id(user_id)
-    return true
-  end
-
-  def authentication_required
-    if @login_user
-      return true
-    else
-      set_error("ログインが必要です。")
-      redirect_to(root_path)
-      return false
-    end
-  end
-
-  def set_notice(message)
-    flash[:notice] = @flash_notice = message
-    flash[:error]  = @flash_error  = nil
-  end
-
-  def set_error(message)
-    flash[:notice] = @flash_notice = nil
-    flash[:error]  = @flash_error  = message
-  end
-
-  def set_error_now(message)
-    flash.now[:notice] = @flash_notice = nil
-    flash.now[:error]  = @flash_error  = message
-  end
-
 end
--- lib/multi_auth.rb
+++ lib/multi_auth.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 class MultiAuth
 
@@ -13,4 +14,51 @@ class MultiAuth
       @user_model.constantize
     end
   end
+
+  module ClassMethods
+    def verify_method_post(options = {})
+      verify({
+               :method => :post,
+               :render => {:text => "Method Not Allowed", :status => 405},
+             }.merge(options))
+    end
+  end
+
+  module InstanceMethods
+
+    private
+
+    def authentication(user_id = session[:user_id])
+      @login_user = User.find_by_id(user_id)
+      return true
+    end
+
+    def authentication_required
+      if @login_user
+        return true
+      else
+        set_error("ログインが必要です。")
+        redirect_to(root_path)
+        return false
+      end
+    end
+
+    def set_notice(message)
+      flash[:notice] = @flash_notice = message
+      flash[:error]  = @flash_error  = nil
+    end
+
+    def set_error(message)
+      flash[:notice] = @flash_notice = nil
+      flash[:error]  = @flash_error  = message
+    end
+
+    def set_error_now(message)
+      flash.now[:notice] = @flash_notice = nil
+      flash.now[:error]  = @flash_error  = message
+    end
+  end
 end
+
+ActionController::Base.__send__(:extend, MultiAuth::ClassMethods)
+ActionController::Base.__send__(:include, MultiAuth::InstanceMethods)
