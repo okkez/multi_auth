e98c79e8bb00bca57b907cd25feaecd7e5a1d7f1
okkez <okkez000@gmail.com>
2009-12-14 00:24:23 +0900

    remove @topic_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  before_filter { |c| c.instance_eval { @topic_path = [] }; true }
 
   GetText.locale = "ja"
   init_gettext "multi_auth"
