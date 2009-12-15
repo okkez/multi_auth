56faaff09a73de868f0eaeeb61bb522dffba91b9
okkez <okkez000@gmail.com>
2009-12-15 17:48:18 +0900

    refactor

--- config/initializers/require_libs.rb
+++ config/initializers/require_libs.rb
@@ -3,6 +3,10 @@
 require 'multi_auth'
 require 'multi_auth_helper'
 MultiAuth.setup do |config|
- config.user_model = 'DummyUser' 
+ config.user_model = 'DummyUser'
 end
+
+ActiveRecord::Base.__send__(:extend, MultiAuth::ActiveRecord::ClassMethods)
+ActionController::Base.__send__(:extend, MultiAuth::ActionController::ClassMethods)
+ActionController::Base.__send__(:include, MultiAuth::ActionController::InstanceMethods)
 
