d70633f8bd39a239952e4a2afdfcbd74b75651cd
okkez <okkez000@gmail.com>
2010-01-05 01:57:29 +0900

    add comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  # multi_auth を使用するアプリケーションには影響しない
   GetText.locale = "ja"
   init_gettext "multi_auth"
 
