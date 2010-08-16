37b94c99813629289fc7fd0a3754086a0a85a8db
okkez <okkez000@gmail.com>
2010-08-16 17:48:54 +0900

    add name credentials list to credentials/index

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 # 認証情報コントローラ
 class CredentialsController < ApplicationController
@@ -6,9 +7,10 @@ class CredentialsController < ApplicationController
 
   # GET /credentials
   def index
-    @open_id_credentials = @login_user.open_id_credentials.all(
-      :order => "open_id_credentials.identity_url ASC")
-    @email_credentials = @login_user.email_credentials.all(
-      :order => "email_credentials.email ASC")
+    @open_id_credentials =
+      @login_user.open_id_credentials.all(:order => "open_id_credentials.identity_url ASC")
+    @email_credentials =
+      @login_user.email_credentials.all(:order => "email_credentials.email ASC")
+    @name_credentials = @login_user.name_credentials.all(:order => "name_credentials.name ASC")
   end
 end
